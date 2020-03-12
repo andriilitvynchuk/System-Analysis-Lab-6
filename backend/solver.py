a6fbf42f2305a1dad1ede1eceb0b752a8b8fbfa1
Andrii Litvynchuk <acethecreator111@gmail.com>
2020-03-12 19:53:27 +0200

    Add empty files for solvers and tests

---
+++ backend/solver.py
@@ -0,0 +1,8 @@
+from typing import NoReturn, List, Any, Dict
+
+import numpy as np
+
+
+class CrossAnalysisSolver:
+    def __init__(self) -> NoReturn:
+        pass
---
+++ test/bruteforce.py
@@ -0,0 +1,15 @@
+import sys
+from typing import NoReturn
+
+
+sys.path.append("..")
+if True:
+    from backend import CrossAnalysisSolver
+
+
+def main() -> NoReturn:
+    pass
+
+
+if __init__ == "__main__":
+    main()
